fcc41cf2f2c5e4074c3a77c29126ae007c10b9b5
Darby Manning <me@darbymanning.com>
2021-04-18 22:35:50 +0100

    Resolves #18 - Resets the app state after an elapsed time from check-in (#22)
    - Makes a new API call after 5 minutes on window focus, to reset the app state
    - The API call is more reliable than simply setting the state open, in case the user focuses the window out of hours, and is then incorrectly shown the check-in view
    - We also display a Button to take the user back to the check-in screen
    - Update babel & jest configs to assist in mocking @sapper/app (we aren't doing this, as it's still problematic even with this fix)
    - Adds a stateResolver

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  moduleFileExtensions: ["js", "svelte"],
+  moduleFileExtensions: ["js", "mjs", "svelte"],
+  moduleDirectories: ["node_modules", "src/node_modules"],
   moduleNameMapper: {
     "^\\$(.*)$": "<rootDir>/src/$1",
   },
@@ -11,10 +12,12 @@ module.exports = {
   testPathIgnorePatterns: [
     "<rootDir>/__sapper__/",
     "<rootDir>/node_modules/",
-    "<rootDir>/src/node_modules/",
+    "<rootDir>/src/node_modules/(?!@sapper)",
   ],
   transform: {
-    "^.+\\.js$": ["babel-jest"],
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.mjs$": "babel-jest",
     "^.+\\.svelte$": ["svelte-jester", { preprocess: true }],
   },
+  transformIgnorePatterns: ["src/node_modules/(?!@sapper)"],
 };
