f18018fd015466ed397687054f5ba477019c944d
Darby Manning <darby@kyan.com>
2021-05-16 22:47:49 +0100

    Adds better resolver for text (#11)

--- src/lib/resolvers/text.js
+++ src/lib/resolvers/text.js
@@ -1,3 +1,23 @@
 export default function (kioskData) {
-  return kioskData.template.languages.translations.en;
+  const { en } = kioskData.template.languages.translations;
+  return {
+    welcomeScreen: {
+      header: en.welcomeScreen.header,
+    },
+    addToQueueForm: {
+      header: en.serviceScreen.header,
+      name: en.customerScreen.placeholder.name,
+      orderNumber: en.customerScreen.placeholder.orderNumber,
+      mobileNumber: en.customerScreen.placeholder.mobileNumber,
+    },
+    inQueue: {
+      header: en.confirmationScreen.header.text.withMobile,
+      currentPosition: en.confirmationScreen.top.text.withMobile,
+      weWillContactYou: en.confirmationScreen.bottom.text.withMobile,
+    },
+    closeScreen: {
+      header: en.closeScreen.header,
+      lineText: en.closeScreen.lineText,
+    },
+  };
 }
--- src/lib/resolvers/text.test.js
+++ src/lib/resolvers/text.test.js
@@ -3,16 +3,68 @@ import textResolver from "./text";
 describe("textResolver", () => {
   it("returns the correct data", () => {
     const data = {
-      id: 1,
       template: {
         languages: {
           translations: {
-            en: "English Text",
+            en: {
+              welcomeScreen: { header: "Welcome Screen Header" },
+              customerScreen: {
+                placeholder: {
+                  name: "Customer Screen Name",
+                  orderNumber: "Customer Screen Order Number",
+                  mobileNumber: "Customer Screen Mobile Number",
+                },
+              },
+              confirmationScreen: {
+                header: {
+                  text: {
+                    withMobile: "Confirmation Screen Header",
+                  },
+                },
+                top: {
+                  text: {
+                    withMobile: "Confirmation Screen Current Position",
+                  },
+                },
+                bottom: {
+                  text: {
+                    withMobile: "Confirmation Screen We Will Contact You",
+                  },
+                },
+              },
+              closeScreen: {
+                header: "Close Screen Header",
+                lineText: "Close Screen Line Text",
+              },
+              serviceScreen: {
+                header: "Service Screen Header",
+              },
+            },
           },
         },
       },
     };
-    const expected = "English Text";
+
+    const expected = {
+      welcomeScreen: {
+        header: "Welcome Screen Header",
+      },
+      addToQueueForm: {
+        header: "Service Screen Header",
+        name: "Customer Screen Name",
+        orderNumber: "Customer Screen Order Number",
+        mobileNumber: "Customer Screen Mobile Number",
+      },
+      inQueue: {
+        header: "Confirmation Screen Header",
+        currentPosition: "Confirmation Screen Current Position",
+        weWillContactYou: "Confirmation Screen We Will Contact You",
+      },
+      closeScreen: {
+        header: "Close Screen Header",
+        lineText: "Close Screen Line Text",
+      },
+    };
     expect(textResolver(data)).toEqual(expected);
   });
 });
