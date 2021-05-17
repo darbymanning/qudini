14f65749304169df428670c882feed1df88e7851
Darby Manning <darby@kyan.com>
2021-05-18 00:27:12 +0100

    Show current 'traffic' #27
    - Adds QueueLengths component
    - Adds getQueueLengthsResolver

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Header } from "./Header";
 export { default as InQueue } from "./InQueue";
 export { default as Footer } from "./Footer";
 export { default as JoinQueueAgain } from "./JoinQueueAgain";
+export { default as QueueLengths } from "./QueueLengths";
